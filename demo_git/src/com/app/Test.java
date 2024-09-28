205f29f1edcf64ae0534daad291439c17c2532be
PratikMane07 <168271590+PratikMane07@users.noreply.github.com>
2024-09-28 11:34:13 +0530

    add m1 method By- Aniket

--- demo_git/src/com/app/Test.java
+++ demo_git/src/com/app/Test.java
@@ -7,6 +7,14 @@ public class Test {
 		System.out.println("this is m1 method");
 	}
 
+	public void m2() {
+		
+		System.out.println("this is m2 method");
+	}
+
+
+	
+
 	public static void main(String[] args) {
 
 	}
